0326248d0e4a1c15740c2208efc0bd75c17ad67c
Khalil <khalil.chettaoui06@gmail.com>
2021-03-26 18:13:18 +0100

    modif yad.sh

--- yad.sh
+++ yad.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
+source ./functions.sh
+source ./menu.sh
 function_g(){
 files=$(yad --width 100 --height 100 --title "Choose the Shell Script" \
       --text="  Choose appropriate option:" \
-      --button="-t:1" \
-      --button="-p:2" \
-      --button="-r:3" \
-      --button="-u:4" \
-      --button="-h:5" \
-      --button="-m:6" \
-      --button="-s:7" \
-      --button="-v:8" \
-      --button="-o:9" \
-      --button="-f:10" \
+      --button="top:1" \
+      --button="ps-aux:2" \
+      --button="root proc:3" \
+      --button="top 5 proc:4" \
+      --button="help:5" \
+      --button="command menu:6" \
+      --button="version:7" \
+      --button="save top proc:8" \
+      --button="display from file:9" \
+      --button="graphic RT:10" \
+      --button="graphic IMG:11" \
+      --button="Neofetch:12" \
       --on-top \
       --center \
   )
-  ret=$?
-  case $ret in
-    1)
-    exec ./KhalilChettaoui.sh -t
-    ;;
-    2)
-    exit 0
-    ;;
-    *)
-    echo "error"
-    exit 1
-    ;;
-  esac
+case ${?} in
+	1) exec top ;;
+	2) ps -aux ;;
+	3) ps -U root -u root u ;;
+	4) watch "ps aux | sort -nrk 3,3 | head -n 5" ;;
+	5) cat help.txt ;;
+	6) function_menu ;;
+	7) cat version.txt ;;
+	8) top -b -n 12 | head -n 12 | sed -n '7,12p' > top-output.txt ;;
+	9) function_of "${2}" "${3}" "${4}" ;;
+	10) gnuplot -p GraphicScript.plt < top-output.txt ;;
+	11) gnuplot -p RTGraph.gnu < top-output.txt	;;
+	12) neofetch ;;
+esac
 }
